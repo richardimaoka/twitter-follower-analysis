4eb3cdb0d8516069ded749b4a1c30509b348ad84
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-03-14 22:56:33 +0900

    unauthorized

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/richardimaoka/twitter-follower-analysis
 
 go 1.15
+
+require github.com/joho/godotenv v1.3.0 // indirect
--- main.go
+++ main.go
@@ -1,23 +1,44 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	UserId := "aaa"
-	resp, err := http.Get(fmt.Sprintf("https://api.twitter.com/2/users/%s/followers?max_results=1000&user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld", UserId))
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	BearerToken := os.Getenv("BEARER_TOKEN")
+
+	UserId := "2875908842"
+	UserFields := "created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld"
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://api.twitter.com/2/users/%s/followers?max_results=1000&user.fields=%s", UserId, UserFields), nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", BearerToken)
+	req.Header.Add("Authorization", "Bearer "+BearerToken)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
 
-	fmt.Println("Response status:", resp.Status)
+	body, err := ioutil.ReadAll(resp.Body)
 
-	scanner := bufio.NewScanner(resp.Body)
-	for i := 0; scanner.Scan() && i < 5; i++ {
-		fmt.Println(scanner.Text())
+	if err != nil {
+		log.Fatal("HTTP failed")
 	}
+	fmt.Printf("%s", body)
 }
