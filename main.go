de9c33eae79c569f87ac94009656efface64b5bf
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-03-17 02:41:26 +0900

    working operation

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func saveJsonToGCSObject(jsonReader io.Reader, bucket, object string) error {
 }
 
 func createGcsBucketIfNotExist(projectId, bucket string) error {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*20)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*50)
 	defer cancel()
 	client, err := storage.NewClient(ctx)
 	if err != nil {
@@ -56,11 +56,11 @@ func createGcsBucketIfNotExist(projectId, bucket string) error {
 	return nil
 }
 
-func fetchJson(userId, bearerToken string) (io.Reader, error) {
+func fetchAndSaveJson(userId, bearerToken, bucket, object string) error {
 	UserFields := "created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld"
 	req, err := http.NewRequest("GET", fmt.Sprintf("https://api.twitter.com/2/users/%s/followers?max_results=1000&user.fields=%s", userId, UserFields), nil)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	req.Header.Add("Authorization", "Bearer "+bearerToken)
@@ -68,11 +68,15 @@ func fetchJson(userId, bearerToken string) (io.Reader, error) {
 	httpClient := &http.Client{}
 	resp, err := httpClient.Do(req)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer resp.Body.Close()
 
-	return resp.Body, nil
+	if err := saveJsonToGCSObject(resp.Body, bucket, object); err != nil {
+		log.Fatalf("Failed to save json to GCS object: %v\n", err)
+	}
+
+	return nil
 }
 
 func main() {
@@ -86,16 +90,11 @@ func main() {
 	userId := "2875908842"
 	object := "twitttt.json"
 
-	jsonReader, err := fetchJson(userId, os.Getenv("BEARER_TOKEN"))
-	if err != nil {
-		log.Fatalf("Failed to fetch json %v\n", err)
-	}
-	a
 	if err := createGcsBucketIfNotExist(projectId, bucket); err != nil {
 		log.Fatalf("Failed to create a GCS bucket: %v\n", err)
 	}
 
-	if err := saveJsonToGCSObject(jsonReader, bucket, object); err != nil {
-		log.Fatalf("Failed to save json to GCS object: %v\n", err)
+	if err := fetchAndSaveJson(userId, os.Getenv("BEARER_TOKEN"), bucket, object); err != nil {
+		log.Fatalf("Failed to fetch and save json %v\n", err)
 	}
 }
