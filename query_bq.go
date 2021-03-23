6dfdcfeac98eabcc68b2031644abd1c5085787a2
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-03-24 01:40:14 +0900

    add a struct tag for bq field name

--- query_bq.go
+++ query_bq.go
@@ -4,11 +4,17 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"log"
 	"os"
 
 	"cloud.google.com/go/bigquery"
+	"github.com/joho/godotenv"
 	"google.golang.org/api/iterator"
 )
+
+type User struct {
+	UserId string `bigquery:"user_id"`
+}
 
 // queryBasic demonstrates issuing a query and reading results.
 func queryBasic(w io.Writer, projectID string) error {
@@ -22,7 +28,7 @@ func queryBasic(w io.Writer, projectID string) error {
 	defer client.Close()
 
 	q := client.Query(
-		"SELECT user_id FROM `richard-twitter-extraction.twitter.user_id")
+		"SELECT user_id FROM `richard-twitter-extraction.twitter.user_ids`")
 	// Location must match that of the dataset(s) referenced in the query.
 	q.Location = "US"
 	// Run the query and print results when the query job is completed.
@@ -39,7 +45,7 @@ func queryBasic(w io.Writer, projectID string) error {
 	}
 	it, err := job.Read(ctx)
 	for {
-		var row []bigquery.Value
+		var row User
 		err := it.Next(&row)
 		if err == iterator.Done {
 			break
@@ -53,7 +59,12 @@ func queryBasic(w io.Writer, projectID string) error {
 }
 
 func main() {
-	err := queryBasic(os.Stdout, "richard-twitter-extraction")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	err = queryBasic(os.Stdout, "richard-twitter-extraction")
 	if err != nil {
 		fmt.Printf("Error in queryBasic %v", err)
 	}
