b5afbb8a6afea9b9b144e97f48ac497199d8b4e4
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-04-13 23:42:00 +0900

    QueryIntoBq func

--- bq_query.go
+++ bq_query.go
@@ -16,6 +16,35 @@ type user struct {
 	UserId string `bigquery:"user_id"`
 }
 
+func QueryIntoBq(ctx context.Context, client *bigquery.Client, cursor int) (string, error) {
+	q := client.Query("SELECT user_id FROM `richard-twitter-extraction.twitter.user_ids`")
+	// Location must match that of the dataset(s) referenced in the query.
+	q.Location = "US"
+
+	// Run the query and print results when the query job is completed.
+	it, err := q.Read(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	//look for the matching row
+	var user User
+	for row := 0; ; row++ {
+		err := it.Next(&user)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return "", err
+		}
+		if row == cursor {
+			return user.UserId, nil
+		}
+	}
+
+	return "", errors.New("No matching row in users BQ table")
+}
+
 // queryBasic demonstrates issuing a query and reading results.
 func queryBq2(cursor int, projectID string) (*User, error) {
 	ctx := context.Background()
@@ -25,44 +54,6 @@ func queryBq2(cursor int, projectID string) (*User, error) {
 	}
 	defer client.Close()
 
-	q := client.Query("SELECT user_id FROM `richard-twitter-extraction.twitter.user_ids`")
-	// Location must match that of the dataset(s) referenced in the query.
-	q.Location = "US"
-
-	// Run the query and print results when the query job is completed.
-	job, err := q.Run(ctx)
-	if err != nil {
-		return nil, err
-	}
-	status, err := job.Wait(ctx)
-	if err != nil {
-		return nil, err
-	}
-	if err := status.Err(); err != nil {
-		return nil, err
-	}
-	it, err := job.Read(ctx)
-
-	//look for the matching row
-	row := 0
-	var user User
-	for {
-		err := it.Next(&user)
-		if err == iterator.Done {
-			break
-		}
-		if err != nil {
-			return nil, err
-		}
-		if row == cursor {
-			break
-		}
-		row++
-	}
-
-	if row < cursor {
-		return nil, errors.New("too big cursor")
-	}
 	return &user, nil
 }
 
@@ -75,18 +66,7 @@ func BqQuery(ctx context.Context, m pubsub.Message) error {
 		log.Fatalf("error unmarshaling json %v", m.Data)
 	}
 
-	bucket := "my-new-buckettttttttttt-francepddan"
-	object := "twitttt.json"
+	queryBq2(0, projectId)
 
-	user, err := queryBq(i.UserCursor, projectId)
-
-	//maybe not needed, and assume that the bucket is creaated beforehand?
-	if err := createGcsBucketIfNotExist(projectId, bucket); err != nil {
-		log.Fatalf("Failed to create a GCS bucket: %v\n", err)
-	}
-
-	if err := fetchAndSaveJson(user.UserId, bearerToken, bucket, object); err != nil {
-		log.Fatalf("Failed to fetch and save json %v\n", err)
-	}
 	return nil
 }
