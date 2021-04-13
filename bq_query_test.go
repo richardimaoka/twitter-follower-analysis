1c6d3f98782fe9f64744ef230eb292177d8ed4ec
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-04-14 00:20:33 +0900

    at least test running

---
+++ bq_query_test.go
@@ -0,0 +1,28 @@
+package p
+
+import (
+	"context"
+	"log"
+	"testing"
+
+	"cloud.google.com/go/bigquery"
+	bqiface "github.com/googleapis/google-cloud-go-testing/bigquery/bqiface"
+	"github.com/joho/godotenv"
+)
+
+func TestIntMinBasic(t *testing.T) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	projectId := "richard-twitter-extraction"
+	ctx := context.Background()
+	c, err := bigquery.NewClient(ctx, projectId)
+	if err != nil {
+		t.Fatal(err)
+	}
+	client := bqiface.AdaptClient(c)
+	defer client.Close()
+
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	cloud.google.com/go/bigquery v1.8.0
 	cloud.google.com/go/pubsub v1.3.1
 	cloud.google.com/go/storage v1.14.0
+	github.com/googleapis/google-cloud-go-testing v0.0.0-20200911160855-bcd43fbb19e8
 	github.com/joho/godotenv v1.3.0
 	google.golang.org/api v0.40.0
 )
