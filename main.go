18811fb1cb73603a9c36eedfc3f7cf2a7962757e
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-04-21 21:50:53 +0900

    change the pakage name from p to functions

--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"context"
+	"log"
 
 	"cloud.google.com/go/pubsub"
+	"github.com/joho/godotenv"
 	"github.com/richardimaoka/twitter-follower-analysis/functions"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	ctx := context.Background()
 	msg := pubsub.Message{}
 	msg.Data = []byte("1")
