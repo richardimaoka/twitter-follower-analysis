2d178d165cd72ee1fa76a8339c9a5344cd297d2e
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-04-22 23:05:31 +0900

    in the middle

--- functions/save_into_gcs.go
+++ functions/save_into_gcs.go
@@ -2,17 +2,44 @@ package functions
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
+
+	"cloud.google.com/go/pubsub"
+	"cloud.google.com/go/storage"
 )
 
-// PubSubMessage is the payload of a Pub/Sub event. Please refer to the docs for
-// additional information regarding Pub/Sub events.
-type PubSubMessage struct {
-	Data []byte `json:"data"`
+func saveOperation1(dirName, fileName string, jsonBytes []byte) error {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	handle := client.Bucket("my-new-buckettttttttttt-francepddan").Object(dirName + "/" + fileName)
+	writer := handle.NewWriter(ctx)
+	defer writer.Close()
+
+	saveOperation2(ctx, writer, jsonBytes)
 }
 
-// HelloPubSub consumes a Pub/Sub message.
-func HelloPubSub(ctx context.Context, m PubSubMessage) error {
-	log.Println(string(m.Data))
+func saveOperation2(ctx context.Context, writer *storage.Writer, jsonBytes []byte) error {
+	if _, err := fmt.Fprintf(writer, "%s", jsonBytes); err != nil {
+		return err
+	}
 	return nil
+}
+
+func SaveIntoGCS(ctx context.Context, m pubsub.Message) error {
+	projectId := os.Getenv("GCP_PROJECT")
+
+	log.Printf("Received a pubsub message: m.Attributes = %s", m.Attributes)
+	log.Printf("Received a pubsub message: m.Data = %s", m.Data)
+
+	err = PublishTwitterFollowings(projectId, message)
+	if err != nil {
+		log.Fatalf("Failed to publish %v\n", err)
+	}
+	log.Printf("published json")
 }
